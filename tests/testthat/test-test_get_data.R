c180eb960c22e6790ec55f6e6bbba3b5c3c17e54
kbstein <kbsteinicke@gmail.com>
2023-04-20 11:13:58 -0600

    Added IndianPines tests for get_data function. Changed get_all_data function to avoid calling get_data

--- tests/testthat/test-test_get_data.R
+++ tests/testthat/test-test_get_data.R
@@ -6,6 +6,14 @@ test_that("get_data", {
   expect_error(get_data("PaviaC", clip_p = 2.5))
   expect_no_error(ip <- get_data("IndianPines", verbose = FALSE))
 
+  ip_data <- get_data("indianpines")
+  expect_equal("IndianPines", ip_data$name)
+  expect_equal(4205000, length(ip_data$img_clipped))
+  expect_equal(c(145, 145, 200), dim(ip_data$img_raw))
+  expect_equal(7457, max(ip_data$img_clipped))
+  expect_equal(1002, min(ip_data$img_clipped))
+
+
   # Add test cases that only verify certain values for indian pines.
   # Check name, check dimensions, check max/min values (can be hard coded)
 
